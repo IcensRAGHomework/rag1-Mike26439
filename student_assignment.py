159238b5351e42aa575de7a5d845f37384aa3502
mike_hsu <mike_hsu@asus.com>
2025-01-06 15:59:33 +0800

    Update student_assignment.py
    part3

--- student_assignment.py
+++ student_assignment.py
@@ -1,81 +1,89 @@
 import json
+import re
 import traceback
 
 from model_configurations import get_model_configuration
 
+from langchain_core.messages import AIMessage
 from langchain_openai import AzureChatOpenAI
 from langchain_core.messages import HumanMessage
 
-#Method HW1 method3
+from langchain_core.output_parsers import PydanticOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.output_parsers.json import SimpleJsonOutputParser
 
-#Method HW1 method 1 & 2
-#from pydantic import BaseModel, Field
-from typing import Optional, Union
-from typing_extensions import Annotated, TypedDict
+from langchain.output_parsers import ResponseSchema, StructuredOutputParser
 
-print_with_method=1
+from typing import List
+from pydantic import BaseModel, Field
 
-class Holiday(TypedDict):
-    """Answer the date and name of holiday."""
+class Holiday(BaseModel):
+    """The date and name of holiday."""
 
-    date: str = Annotated[str, ..., "The date of the holiday, must be YYYY-mm-DD format"]
-    name: str = Annotated[str, ..., "The short name of the holiday"]
+    date: str = Field(..., description="The date of the holiday, must be YYYY-mm-DD format")
+    name: str = Field(..., description="The short name of the holiday")
 
-class FinalResponse(TypedDict):
-    Result: Union[Holiday]
+class Result(BaseModel):
+    Result: List[Holiday]
 
+# Custom parser
+def extract_json(message: AIMessage) -> List[dict]:
+    """Extracts JSON content from a string where JSON is embedded between \`\`\`json and \`\`\` tags.
+
+    Parameters:
+        text (str): The text containing the JSON content.
+
+    Returns:
+        list: A list of extracted JSON strings.
+    """
+    text = message.content
+    # Define the regular expression pattern to match JSON blocks
+    pattern = r"\`\`\`json(.*?)\`\`\`"
+
+    # Find all non-overlapping matches of the pattern in the string
+    matches = re.findall(pattern, text, re.DOTALL)
+
+    # Return the list of matched JSON strings, stripping any leading or trailing whitespace
+    try:
+        return [json.loads(match.strip()) for match in matches]
+    except Exception:
+        raise ValueError(f"Failed to parse: {message}")
+
+# Set up a parser
+parser = PydanticOutputParser(pydantic_object=Result)
+
+####################################################
 gpt_chat_version = 'gpt-4o'
 gpt_config = get_model_configuration(gpt_chat_version)
 
 def generate_hw01(question):
     responseStr=""
-    if print_with_method == 1:
-        print("Output with method 1")
-        llm = AzureChatOpenAI(
-            model=gpt_config['model_name'],
-            deployment_name=gpt_config['deployment_name'],
-            openai_api_key=gpt_config['api_key'],
-            openai_api_version=gpt_config['api_version'],
-            azure_endpoint=gpt_config['api_base'],
-            temperature=gpt_config['temperature']
-        )
-        structured_llm = llm.with_structured_output(FinalResponse)
-        response = structured_llm.invoke(question)
-        responseStr = json.dumps(response, ensure_ascii=False)
-    elif print_with_method == 2:
-        print("Output with method 2")
-        llm = AzureChatOpenAI(
-            model=gpt_config['model_name'],
-            deployment_name=gpt_config['deployment_name'],
-            openai_api_key=gpt_config['api_key'],
-            openai_api_version=gpt_config['api_version'],
-            azure_endpoint=gpt_config['api_base'],
-            temperature=gpt_config['temperature']
-        )
-        model_with_tools = llm.bind_tools([FinalResponse])
-        response = model_with_tools.invoke(question)
-        responseStr = json.dumps(response.tool_calls[0]["args"], ensure_ascii=False)
-    else:
-        print("Output with method 3")
-        llm = AzureChatOpenAI(
-            model=gpt_config['model_name'],
-            deployment_name=gpt_config['deployment_name'],
-            openai_api_key=gpt_config['api_key'],
-            openai_api_version=gpt_config['api_version'],
-            azure_endpoint=gpt_config['api_base'],
-            temperature=gpt_config['temperature'],
-            model_kwargs={ "response_format": { "type": "json_object" } }
-        )
-        prompt = ChatPromptTemplate.from_template(
-            "Answer the user's question to the best of your ability."
-            'You must always output a JSON object with an "date" key which format is YYYY-mm-DD and a "name" key which is the name of holiday.'
-            "{question}"
-        )
-        chain = prompt | llm | SimpleJsonOutputParser()
-        response = chain.invoke({ "question": question })
-        responseStr = json.dumps(response, ensure_ascii=False)
+    llm = AzureChatOpenAI(
+        model=gpt_config['model_name'],
+        deployment_name=gpt_config['deployment_name'],
+        openai_api_key=gpt_config['api_key'],
+        openai_api_version=gpt_config['api_version'],
+        azure_endpoint=gpt_config['api_base'],
+        temperature=gpt_config['temperature']
+    )
+
+    # Prompt
+    prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "Answer the user question."
+            "Output as JSON without description."
+            "With one Result object which conatins multiple object with date key and name key."
+        ),
+        ("human", "{question}"),
+    ]
+    )
+    #print(prompt.format_prompt(question=question).to_string())
+
+    chain = prompt | llm
+    response = chain.invoke({ "question": question })
+    responseStr = extract_json(response)
     return responseStr
     
 def generate_hw02(question):
@@ -105,4 +113,6 @@ def demo(question):
     return response
 
 
+#print(demo("2024年台灣10月紀念日有哪些?"))
+#generate_hw01("2024年台灣10月紀念日有哪些?")
 #print(generate_hw01("2024年台灣10月紀念日有哪些?"))
